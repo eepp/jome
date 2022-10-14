0f48f1b165c100a2fd0ff313111f52d1c3df2f77
Philippe Proulx <eeppeliteloop@gmail.com>
2022-10-14 11:15:18 -0400

    gen-data/create.py: fix error message
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- gen-data/create.py
+++ gen-data/create.py
@@ -281,7 +281,7 @@ def _main(output_dir):
         for emoji in emojis:
             if emoji not in done_emojis:
                 if emoji not in all_emoji_descrs:
-                    _error('Cannot find `{}` in `emoji.json`.'.format(emoji))
+                    _error('Cannot find `{}` in the `emojis-*.txt` files.'.format(emoji))
 
                 emoji_descriptors.append(all_emoji_descrs[emoji])
                 done_emojis.add(emoji)
